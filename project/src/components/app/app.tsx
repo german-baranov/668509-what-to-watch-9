2e77bb60613955d7f9273c4e07d39b83e7a6bd57
German Baranov <german.baranov@geotoolkit.net>
2022-02-22 15:45:30 +0700

    feat: move MyList to PrivateRoute

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,13 +1,14 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import { IAppStore } from '../../types/appstore.types';
 import {Main} from '../main/main';
-import {AppRoute} from '../../const';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import { SignIn } from '../sign-in/sign-in';
 import { MyList } from '../my-list/my-list';
 import { MoviePage } from '../movie-page/movie-page';
 import { Player } from '../player/player';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
 import { AddReview } from '../add-review/add-review';
+import PrivateRoute from '../private-route/private-route';
 
 export function App({...args}: IAppStore): JSX.Element {
   return (
@@ -15,7 +16,12 @@ export function App({...args}: IAppStore): JSX.Element {
       <Routes>
         <Route path={AppRoute.Main} element={<Main {...args}/>}/>
         <Route path={AppRoute.SignIn} element={<SignIn/>}/>
-        <Route path={AppRoute.MyList} element={<MyList/>}/>
+        <Route path={AppRoute.MyList} element={
+          <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+            <MyList/>
+          </PrivateRoute>
+        }
+        />
         <Route path={AppRoute.Film} element={<MoviePage/>}/>
         <Route path={AppRoute.AddReview} element={<AddReview/>}/>
         <Route path={AppRoute.Player} element={<Player/>}/>
